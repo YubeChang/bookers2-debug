3513b1e961810cc6ca108b18f8a18a8ae12e90fe
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-06 13:10:40 +0000

    JSの勉強するから中断

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,24 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
+    # redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
+     # redirect_to request.referer
   end
-
 end
