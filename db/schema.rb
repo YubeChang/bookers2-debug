57166876ec7dc4e5985f89c3e335878342311ff2
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-08 14:36:32 +0000

    fix Ajax-fav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_045529) do
+ActiveRecord::Schema.define(version: 2022_04_08_124457) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_045529) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "book_id"], name: "index_favorites_on_user_id_and_book_id", unique: true
   end
 
   create_table "relationships", force: :cascade do |t|
