189800e26e35e4a254cc7a13c83a49737b6c444e
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-09 02:09:04 +0000

    install chart.js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,30 @@ class UsersController < ApplicationController
     @time = Time.now
     @today =     Book.where(user_id: @user.id,
                          created_at: @time.in_time_zone.all_day)
-    @day1 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(1.days).all_day)
-    @day2 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(2.days).all_day)
-    @day3 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(3.days).all_day)
-    @day4 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(4.days).all_day)
-    @day5 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(5.days).all_day)
-    @day6 =      Book.where(user_id: @user.id,
-                         created_at: @time.in_time_zone.ago(6.days).all_day)
-    # 絶対間違ってる、これ過去の履歴作る時全部手書きすることになるやんけ。creaated_atでwhereする方法ないかな
+  # これをリファクタリングしたい
+    # @day1 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(1.days).all_day)
+    # @day2 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(2.days).all_day)
+    # @day3 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(3.days).all_day)
+    # @day4 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(4.days).all_day)
+    # @day5 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(5.days).all_day)
+    # @day6 =      Book.where(user_id: @user.id,
+    #                     created_at: @time.in_time_zone.ago(6.days).all_day)
+  # 絶対間違ってる、これ過去の履歴作る時全部手書きすることになるやんけ。creaated_atでwhereする方法ないかな
+
+  #リファクタリング案１<動的メソッド作成>
+  #スッキリはしたけどこれだと「何日分のデータが欲しい」って時に書き直さないと検索できない
+    6.times do |i|
+      instance_variable_set("@day"+(i + 1).to_s,
+                            Book.where(user_id: @user.id,
+                                    created_at: @time.in_time_zone.ago((i + 1).days).all_day)
+                            )
+    end
+
   end
 
   def index
